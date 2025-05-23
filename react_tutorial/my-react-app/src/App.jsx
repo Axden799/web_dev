63e3f468eecdde52b1f6139bb163d42f08718bfe
Denis Dutka <dedutka@gmail.com>
2025-05-23 15:02:14 -0400

    Complete React tutorial.

--- react_tutorial/my-react-app/src/App.jsx
+++ react_tutorial/my-react-app/src/App.jsx
@@ -4,30 +4,80 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+function MyButton() {
+  return (
+    <div>
+      <h1>This is a button</h1>
+      <button>I'm a button</button>
+    </div>
+  );
+}
+
+function AlertButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
+}
+
+function StateButton({ count, onClick }) {
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
+
+function ProductList() {
+  const products = [
+    {title: 'Stratocaster', isGuitar: true, id:'1'},
+    {title: 'Les Paul', isGuitar: true, id:'2'},
+    {title: 'Yamaha Piano', isGuitar: false, id:'3'},
+  ];
+
+  const listItems = products.map(product => 
+    <li
+      key={product.id}
+      style={{
+        color: product.isGuitar ? 'magenta' : 'dargreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
+  return (
+    <div>
+      <ul>{listItems}</ul>
+    </div>
+  )
+}
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+
+      <p>Here are a list of products</p>
+      <ProductList />
+
+      <p>This is an alert button!</p>
+      <AlertButton />
+
+      <p>These are state buttons</p>
+      <StateButton count={count} onClick={handleClick} />
+      <StateButton count={count} onClick={handleClick} />
     </>
   )
 }
