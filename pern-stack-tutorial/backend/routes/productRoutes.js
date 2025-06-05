e90de2582fd2950465c20cd8802ef406b345a955
Denis Dutka <dedutka@gmail.com>
2025-06-05 13:17:30 -0400

    The productRoutes.js can have all of the routes used by products, such as CRUD to create, delete, update, and retrieve the products.

--- pern-stack-tutorial/backend/routes/productRoutes.js
+++ pern-stack-tutorial/backend/routes/productRoutes.js
@@ -2,8 +2,12 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-    res.send("test route");
+router.get("/", (req, res) => {
+    // GET ALL PRODUCTS
+});
+
+router.post("/create", (req, res) => {
+    // CREATE A PRODUCTS
 });
 
 export default router;
