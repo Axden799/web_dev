f6729f608587c556a1af4a0a93297f5e5de6d0f2
Denis Dutka <dedutka@gmail.com>
2025-06-05 14:47:58 -0400

    Add db.js code. Creates an sql function that uses the environment variables to connect to the PGSQL database in neon using the custom URL. In server.js, created an async function that will create the database products with the appropriate columns in a try catch block with a success string and an error string. Created a .then function that first calls initDB().then() to listen on the port after we have created our database.

--- pern-stack-tutorial/backend/server.js
+++ pern-stack-tutorial/backend/server.js
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 
 import productRoutes from "./routes/productRoutes.js";
+import { sql } from "./config/db.js";
 
 dotenv.config(); // apply the config from the .env file using dotenv that contains server and database information
 
@@ -19,6 +20,27 @@ app.use(morgan("dev")) // log the requests to the console GET /test 200 1.955 ms
 
 app.use("/api/products", productRoutes); // the route /api/products will use the routes from the productRoutes.js file
 
-app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT);
-});
+async function initDB() {
+    try {
+        await sql`
+            CREATE TABLE IF NOT EXISTS products(
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                image VARCHAR(255) NOT NULL,
+                price DECIMAL(10, 2) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `;
+    
+    console.log("Database initialized succesfully!");
+    } catch (error) {
+        console.log("Error initDB", error);
+    }
+}
+
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server is running on port " + PORT);
+    });
+})
