0affc8548eea043f34576c986d9ed2e067abf1d3
Denis Dutka <dedutka@gmail.com>
2025-06-05 12:31:42 -0400

    Add middleware packages. Add cors to app to communicate between backend and frontend ports, add helmet to app to add security headers to our HTTP requests and responses, add morgan to log HTTP traffic to the console under dev state to use only for development. Create a default route at root / to test the server. Create a route at /test to test the /test route and print a message and test helmet and morgan. To test helmet, we print the res.getHeaders() to the console.

--- pern-stack-tutorial/backend/server.js
+++ pern-stack-tutorial/backend/server.js
@@ -1,6 +1,21 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(express.json()); // extract data from database as JSON to parse in our app
+app.use(cors()); //  Middleware to enable Cross-Origin Resource Sharing. This is essential for your frontend (running on a different port/origin) to communicate with your backend.
+app.use(helmet()) // helmet is a security middleware that helps you protect your app by setting various HTTP headers
+app.use(morgan("dev")) // log the requests to the console GET /test 200 1.955 ms - 27 and GET /favicon.ico 404 0.711 ms - 150
+// if we hard refresh CTRL + SHIFT + R, we will get response 200 (OK). If we simply refresh, we will get response 304, (OK from cache)
+
+
+app.get("/test", (req, res) => {
+    console.log(res.getHeaders());
+    res.send("Hello, from the test route!");
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
