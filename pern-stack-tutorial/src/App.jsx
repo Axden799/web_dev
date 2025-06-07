3f62ab80742d50452f1910c87c24c75c14086a7c
Denis Dutka <dedutka@gmail.com>
2025-06-07 18:12:18 -0400

    Finish NavBar. Create the shopping bag icon with a number only shown in the / page. Create a store for the currently used theme from localStorage. Use that to highlight the currently set theme and pass it to global state using zustand. Create a dropdown for the themes along with each color as denoted in the constants directory which contains a list of all the themes and their 3 major colors.

--- pern-stack-tutorial/src/App.jsx
+++ pern-stack-tutorial/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react';
 import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import {useThemeStore} from "./store/useThemeStore";
 
 import { Routes, Route } from "react-router-dom";
 
 
 function App() {
-
+  const {theme} = useThemeStore();
   return (
-      <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme="cupcake">
+      <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
         <NavBar />
 
         <Routes>
--- pern-stack-tutorial/src/components/NavBar.jsx
+++ pern-stack-tutorial/src/components/NavBar.jsx
@@ -1,7 +1,11 @@
-import { ShoppingCartIcon } from "lucide-react";
-import { Link } from "react-router-dom";
+import { ShoppingCartIcon, ShoppingBagIcon } from "lucide-react";
+import { Link, useResolvedPath } from "react-router-dom";
+import ThemeSelector from "./ThemeSelector";
 
 function NavBar() {
+    const {pathname} = useResolvedPath();
+    const isHomePage = pathname === "/";
+
     return (
         <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50">NavBar
             <div className="max-w-7xl mx-auto">
@@ -19,6 +23,20 @@ function NavBar() {
                                 </span>
                             </div>
                         </Link>
+                    </div>
+
+                    {/*RIGHT SECTION */}
+                    <div className="flex items-center gap-4">
+                        <ThemeSelector />
+
+                        {isHomePage && (
+                            <div className="indicator">
+                                <div className="p-2 rounded-full hover:bg-base-200 transition-colors">
+                                    <ShoppingBagIcon className="size-5" />
+                                    <span className="badge badge-sm badge-primary indicator-item">8</span>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
