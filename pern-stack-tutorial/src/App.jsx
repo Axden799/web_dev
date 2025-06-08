16fde56922a1b7e26e07fbe4b2fa2c79b517b7d7
Denis Dutka <dedutka@gmail.com>
2025-06-08 12:32:41 -0400

    Added modal, product add and delete. Will not continue this project as lecturer chose to use zustand instead of React. will find another tutorial instead.

--- pern-stack-tutorial/src/App.jsx
+++ pern-stack-tutorial/src/App.jsx
@@ -4,6 +4,8 @@ import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import {useThemeStore} from "./store/useThemeStore";
 
+import { Toaster } from "react-hot-toast";
+
 import { Routes, Route } from "react-router-dom";
 
 
@@ -17,6 +19,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/product/:id" element={<ProductPage />} />
         </Routes>
+
+        <Toaster />
       </div>
   );
 }
