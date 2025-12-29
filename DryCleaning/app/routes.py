4ecaa6f883ca221cd3982d28dfb1c1c21a5f1608
Denis Dutka <dedutka@gmail.com>
2025-12-29 01:17:40 -0500

    Create template files in HTML for Flask function output. Create a base template containing headers and footers. Create a config file with env variables. Source config file from Flask Class Object.

--- DryCleaning/app/routes.py
+++ DryCleaning/app/routes.py
@@ -1,6 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
+    user = {'username': 'Denis'}
+    return render_template('index.html', title='Home', user=user)
     return "Hello, World!"
+
