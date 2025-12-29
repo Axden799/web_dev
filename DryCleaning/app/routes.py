2001502f48752e0543d71ef3ef692f36e8b5da83
Denis Dutka <dedutka@gmail.com>
2025-12-29 01:33:31 -0500

    Login form route with HTML form.

--- DryCleaning/app/routes.py
+++ DryCleaning/app/routes.py
@@ -1,10 +1,14 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Denis'}
     return render_template('index.html', title='Home', user=user)
-    return "Hello, World!"
 
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
