438b190f5dd988ad072457029c541236f2ee3991
Denis Dutka <dedutka@gmail.com>
2026-01-09 13:57:50 -0500

    Add language support for Spanish using flask-babel. Add _ and _l for lazy loading of all text that needs to be translated. Generate the language file, compile it, and apply it to the site. Change the language to Spanish to test if it works. Use get_locale() to get the dates on the site translated to the specified language. Create a cli.py file to store these long flask-babel commands using click to generate a command-line argument macro for them.

--- DryCleaning/app/__init__.py
+++ DryCleaning/app/__init__.py
@@ -6,16 +6,25 @@ from flask_login import LoginManager
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
 from flask_moment import Moment
+from flask import request
+from flask_babel import Babel, lazy_gettext as _l
 
 import os, logging
+
+def get_locale():
+    #return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return 'es'
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 moment = Moment(app)
+babel = Babel(app, locale_selector=get_locale)
 
 # tells Flask which view handles login
 login.login_view = 'login'
--- DryCleaning/config.py
+++ DryCleaning/config.py
@@ -6,6 +6,8 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    LANGUAGES = ['en', 'es']
     
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
